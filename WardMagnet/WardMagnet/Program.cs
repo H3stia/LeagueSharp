28907cbf9a913c0abc9a7b3ea58675edb247375e
Hestia-BoL <davi.francio@gmail.com>
2014-09-29 13:43:45 +0200

    Ward Magnet by Screeder
    Ward Magnet from SAwareness

---
+++ WardMagnet/WardMagnet/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using SharpDX.Direct3D9;
+using SharpDX.Toolkit.Diagnostics;
+using MenuItem = LeagueSharp.Common.MenuItem;
+
+namespace WardMagnet
+{
+    static class Menu
+    {
+        public class MenuItemSettings
+        {
+            public String Name;
+            public dynamic Item;
+            public Type Type;
+            public bool ForceDisable;
+            public LeagueSharp.Common.Menu Menu;
+            public List<MenuItemSettings> SubMenus = new List<MenuItemSettings>();
+            public List<MenuItem> MenuItems = new List<MenuItem>();
+
+            public MenuItemSettings(Type type, dynamic item)
+            {
+                Type = type;
+                Item = item;
+            }
+
+            public MenuItemSettings(dynamic item)
+            {
+                Item = item;
+            }
+
+            public MenuItemSettings(Type type)
+            {
+                Type = type;
+            }
+
+            public MenuItemSettings(String name)
+            {
+                Name = name;
+            }
+
+            public MenuItemSettings()
+            {
+
+            }
+
+            public MenuItemSettings AddMenuItemSettings(String displayName, String name)
+            {
+                SubMenus.Add(new Menu.MenuItemSettings(name));
+                MenuItemSettings tempSettings = GetMenuSettings(name);
+                if (tempSettings == null)
+                {
+                    throw new NullReferenceException(name + " not found");
+                }
+                tempSettings.Menu = Menu.AddSubMenu(new LeagueSharp.Common.Menu(displayName, name));
+                return tempSettings;
+            }
+
+            public bool GetActive()
+            {
+                if (Menu == null)
+                    return false;
+                foreach (var item in Menu.Items)
+                {
+                    if (item.DisplayName == "Active")
+                    {
+                        if (item.GetValue<bool>())
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return false;
+            }
+
+            public void SetActive(bool active)
+            {
+                if (Menu == null)
+                    return;
+                foreach (var item in Menu.Items)
+                {
+                    if (item.DisplayName == "Active")
+                    {
+                        item.SetValue(active);
+                        return;
+                    }
+                }
+            }
+
+            public MenuItem GetMenuItem(String menuName)
+            {
+                if (Menu == null)
+                    return null;
+                foreach (var item in Menu.Items)
+                {
+                    if (item.Name == menuName)
+                    {
+                        return item;
+                    }
+                }
+                return null;
+            }
+
+            public LeagueSharp.Common.Menu GetSubMenu(String menuName)
+            {
+                if (Menu == null)
+                    return null;
+                return Menu.SubMenu(menuName);
+            }
+
+            public MenuItemSettings GetMenuSettings(String name)
+            {
+                foreach (var menu in SubMenus)
+                {
+                    if (menu.Name.Contains(name))
+                        return menu;
+                }
+                return null;
+            }
+        }
+        public static MenuItemSettings Wards = new MenuItemSettings();
+        public static MenuItemSettings WardCorrector = new MenuItemSettings(typeof(WardMagnet.WardCorrector));
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void CreateMenu()
+        {
+            {
+                LeagueSharp.Common.Menu menu = new LeagueSharp.Common.Menu("Ward Magnet", "WardMagnet", true);
+
+
+                Menu.Wards.Menu = menu.AddSubMenu(new LeagueSharp.Common.Menu("Ward Magnet", "WardMagnet"));
+                Menu.WardCorrector.Menu = Menu.Wards.Menu.AddSubMenu(new LeagueSharp.Common.Menu("WardCorrector", "WardCorrector"));
+                Menu.WardCorrector.MenuItems.Add(Menu.WardCorrector.Menu.AddItem(new LeagueSharp.Common.MenuItem("WardCorrectorActive", "Active").SetValue(true)));
+
+                menu.AddItem(new LeagueSharp.Common.MenuItem("By Screeder", "By Screeder "));
+                menu.AddToMainMenu();
+            }
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            try
+            {
+                CreateMenu();
+                Game.PrintChat("Ward Magnet loaded!");
+                Game.OnGameUpdate += GameOnOnGameUpdate;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ward Magnet: " + e.ToString());
+            }
+        }
+
+        private static void GameOnOnGameUpdate(EventArgs args)
+        {
+            Type classType = typeof(Menu);
+            BindingFlags flags = BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly;
+            FieldInfo[] fields = classType.GetFields(flags);
+            foreach (FieldInfo p in fields)
+            {
+                var item = (Menu.MenuItemSettings)p.GetValue(null);
+                if (item.GetActive() == false && item.Item != null)
+                {
+                    item.Item = null;
+                }
+                else if (item.GetActive() && item.Item == null && !item.ForceDisable && item.Type != null)
+                {
+                    try
+                    {
+                        item.Item = System.Activator.CreateInstance(item.Type);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ WardMagnet/WardMagnet/Utils.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Resources;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using SharpDX.Direct3D9;
+using SharpDX.Toolkit.Graphics;
+using Color = SharpDX.Color;
+using Font = SharpDX.Direct3D9.Font;
+using PrimitiveType = SharpDX.Direct3D9.PrimitiveType;
+using Rectangle = SharpDX.Rectangle;
+using Texture = SharpDX.Direct3D9.Texture;
+
+namespace WardMagnet
+{
+    static class Common
+    {
+        public static bool IsOnScreen(Vector3 vector)
+        {
+            Vector2 screen = Drawing.WorldToScreen(vector);
+            if (screen[0] < 0 || screen[0] > Drawing.Width || screen[1] < 0 || screen[1] > Drawing.Height)
+                return false;
+            return true;
+        }
+
+        public static bool IsOnScreen(Vector2 vector)
+        {
+            Vector2 screen = vector;
+            if (screen[0] < 0 || screen[0] > Drawing.Width || screen[1] < 0 || screen[1] > Drawing.Height)
+                return false;
+            return true;
+        }
+
+        public static Size ScaleSize(this Size size, float scale, Vector2 mainPos = default(Vector2))
+        {
+            size.Height = (int)(((size.Height - mainPos.Y) * scale) + mainPos.Y);
+            size.Width = (int)(((size.Width - mainPos.X) * scale) + mainPos.X);
+            return size;
+        }
+
+        public static bool IsInside(Vector2 mousePos, Size windowPos, int width, int height)
+        {
+            return Utils.IsUnderRectangle(mousePos, windowPos.Width, windowPos.Height, width, height);
+        }
+    }
+
+
+    static class DirectXDrawer
+    {
+
+        public struct PositionColored
+        {
+            public static readonly VertexFormat FVF = VertexFormat.Position | VertexFormat.Diffuse;
+            public static readonly int Stride = Vector3.SizeInBytes + sizeof(int);
+
+            public Vector3 Position;
+            public int Color;
+
+            public PositionColored(Vector3 pos, int col)
+            {
+                Position = pos;
+                Color = col;
+            }
+        }
+
+        private static void InternalRender(Vector3 target)
+        {
+            Drawing.Direct3DDevice.VertexShader = null;
+            Drawing.Direct3DDevice.PixelShader = null;
+            Drawing.Direct3DDevice.SetRenderState(RenderState.AlphaBlendEnable, true);
+            Drawing.Direct3DDevice.SetRenderState(RenderState.BlendOperation, BlendOperation.Add);
+            Drawing.Direct3DDevice.SetRenderState(RenderState.DestinationBlend, Blend.InverseSourceAlpha);
+            Drawing.Direct3DDevice.SetRenderState(RenderState.SourceBlend, Blend.SourceAlpha);
+            Drawing.Direct3DDevice.SetRenderState(RenderState.Lighting, 0);
+            Drawing.Direct3DDevice.SetRenderState(RenderState.ZEnable, true);
+            Drawing.Direct3DDevice.SetRenderState(RenderState.AntialiasedLineEnable, true);
+            Drawing.Direct3DDevice.SetRenderState(RenderState.Clipping, true);
+            Drawing.Direct3DDevice.SetRenderState(RenderState.EnableAdaptiveTessellation, true);
+            Drawing.Direct3DDevice.SetRenderState(RenderState.MultisampleAntialias, true);
+            Drawing.Direct3DDevice.SetRenderState(RenderState.ShadeMode, ShadeMode.Gouraud);
+            Drawing.Direct3DDevice.SetTexture(0, null);
+            Drawing.Direct3DDevice.SetRenderState(RenderState.CullMode, Cull.None);
+        }
+
+        public static void DrawLine(Vector3 from, Vector3 to, System.Drawing.Color color)
+        {
+            var vertices = new PositionColored[2];
+            vertices[0] = new PositionColored(Vector3.Zero, color.ToArgb());
+            from = from.SwitchYZ();
+            to = to.SwitchYZ();
+            vertices[1] = new PositionColored(to - from, color.ToArgb());
+
+            InternalRender(from);
+
+            Drawing.Direct3DDevice.DrawUserPrimitives(PrimitiveType.LineList, vertices.Length / 2, vertices);
+        }
+
+        public static void DrawLine(Line line, Vector3 from, Vector3 to, ColorBGRA color, Size size = default(Size), float[] scale = null, float rotation = 0.0f)
+        {
+            if (line != null)
+            {
+                from = from.SwitchYZ();
+                to = to.SwitchYZ();
+                Matrix nMatrix = (scale != null ? Matrix.Scaling(scale[0], scale[1], 0) : Matrix.Scaling(1)) *
+                                 Matrix.RotationZ(rotation) * Matrix.Translation(from);
+                Vector3[] vec = { from, to };
+                line.DrawTransform(vec, nMatrix, color);
+            }
+
+        }
+    }
+}
