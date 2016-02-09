7ed15b692fac57d2078d98804b1f15011921026a
Hestia <davi.francio@gmail.com>
2016-02-09 19:34:58 +0100

    Update 6.2: - Rewrote most of the code - Fixed E usage on AfterAttack - Added tiamat/ravenous/titanic usage - Better W usage - Switched to SPrediction (credits to Shine & Synx) - Fixed JungleClear and moved it on LaneClear hotkey - Fixed AutoQ usage

---
+++ Mundo/Mundo/CommonUtilities.cs
@@ -0,0 +1,39 @@
+﻿using LeagueSharp;
+using ItemData = LeagueSharp.Common.Data.ItemData;
+
+namespace Mundo
+{
+    internal class CommonUtilities
+    {
+        public static Obj_AI_Hero Player
+        {
+            get { return ObjectManager.Player; }
+        }
+
+        public static bool CheckItem()
+        {
+            return ItemData.Tiamat_Melee_Only.GetItem().IsReady() ||
+                   ItemData.Ravenous_Hydra_Melee_Only.GetItem().IsReady() ||
+                   ItemData.Titanic_Hydra_Melee_Only.GetItem().IsReady();
+        }
+
+        public static void UseItem()
+        {
+            if (!CheckItem())
+                return;
+
+            if (ItemData.Tiamat_Melee_Only.GetItem().IsOwned(Player))
+            {
+                ItemData.Tiamat_Melee_Only.GetItem().Cast();
+            }
+            if (ItemData.Ravenous_Hydra_Melee_Only.GetItem().IsOwned(Player))
+            {
+                ItemData.Ravenous_Hydra_Melee_Only.GetItem().Cast();
+            }
+            if (ItemData.Titanic_Hydra_Melee_Only.GetItem().IsOwned(Player))
+            {
+                ItemData.Titanic_Hydra_Melee_Only.GetItem().Cast();
+            }
+        }
+    }
+}
---
+++ Mundo/Mundo/Drawings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Color = System.Drawing.Color;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Mundo
+{
+    internal class Drawings : Spells
+    {
+        public static void OnDraw(EventArgs args)
+        {
+            if (CommonUtilities.Player.IsDead || ConfigMenu.config.Item("disableDraw").GetValue<bool>())
+                return;
+
+            var heroPosition = Drawing.WorldToScreen(CommonUtilities.Player.Position);
+            var textDimension = Drawing.GetTextExtent("AutoQ: ON");
+            var drawQstatus = ConfigMenu.config.Item("drawAutoQ").GetValue<bool>();
+            var autoQ = ConfigMenu.config.Item("autoQ").GetValue<KeyBind>().Active;
+
+            if (drawQstatus && autoQ)
+            {
+                Drawing.DrawText(heroPosition.X - textDimension.Width, heroPosition.Y - textDimension.Height, Color.DarkOrange, "AutoQ: ON");
+            }
+
+            var width = ConfigMenu.config.Item("width").GetValue<Slider>().Value;
+
+            if (ConfigMenu.config.Item("drawQ").GetValue<Circle>().Active && q.Level > 0)
+            {
+                var circle = ConfigMenu.config.Item("drawQ").GetValue<Circle>();
+                Render.Circle.DrawCircle(CommonUtilities.Player.Position, circle.Radius, circle.Color, width);
+            }
+
+            if (ConfigMenu.config.Item("drawW").GetValue<Circle>().Active && w.Level > 0)
+            {
+                var circle = ConfigMenu.config.Item("drawW").GetValue<Circle>();
+                Render.Circle.DrawCircle(CommonUtilities.Player.Position, circle.Radius, circle.Color, width);
+            }
+        }
+    }
+}
