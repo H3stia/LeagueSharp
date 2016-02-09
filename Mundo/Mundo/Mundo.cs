6bd71cf805438f9c6880e1b8500975f968ecdb05
Hestia <davi.francio@gmail.com>
2016-02-09 20:42:54 +0100

    Update: - Fixed compiling error

---
+++ Mundo/Mundo/Mundo.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SPrediction;
+
+namespace Mundo
+{
+    internal class Mundo : Spells
+    {
+
+        public Mundo()
+        {
+            CustomEvents.Game.OnGameLoad += OnLoad;
+        }
+
+        private static void OnLoad(EventArgs args)
+        {
+            if (ObjectManager.Player.ChampionName != "DrMundo")
+                return;
+
+            InitializeSpells();
+            ConfigMenu.InitializeMenu();
+
+            Game.OnUpdate += OnUpdate;
+            Orbwalking.AfterAttack += AfterAttack;
+            Drawing.OnDraw += Drawings.OnDraw;
+
+            SPrediction.Prediction.Initialize(ConfigMenu.config);
+
+            Notifications.AddNotification("Dr.Mundo by Hestia loaded!", 5000);
+        }
+
+        private static void AfterAttack(AttackableUnit unit, AttackableUnit target)
+        {
+            if ((ConfigMenu.orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo || ConfigMenu.orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed) && unit.IsMe)
+            {
+                if (ConfigMenu.config.Item("useE").GetValue<bool>() && e.IsReady() && target is Obj_AI_Hero && target.IsValidTarget(e.Range))
+                {
+                    e.Cast();
+                }
+
+            }
+
+            if (ConfigMenu.orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear && unit.IsMe)
+            {
+                if (ConfigMenu.config.Item("useEj").GetValue<bool>() && e.IsReady() && target is Obj_AI_Minion && target.IsValidTarget(e.Range))
+                {
+                    e.Cast();
+                }
+
+            }
+
+            if ((ConfigMenu.orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo ||
+                 ConfigMenu.orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed) && unit.IsMe)
+            {
+                if ((ConfigMenu.config.Item("titanicC").GetValue<bool>() || ConfigMenu.config.Item("ravenousC").GetValue<bool>() ||
+                     ConfigMenu.config.Item("tiamatC").GetValue<bool>()) && !e.IsReady() && target is Obj_AI_Hero &&
+                    target.IsValidTarget(e.Range) && CommonUtilities.CheckItem())
+                {
+                    CommonUtilities.UseItem();
+                }
+
+            }
+
+            if (ConfigMenu.orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear && unit.IsMe)
+            {
+                if ((ConfigMenu.config.Item("titanicF").GetValue<bool>() || ConfigMenu.config.Item("ravenousF").GetValue<bool>() ||
+                     ConfigMenu.config.Item("tiamatF").GetValue<bool>()) && !e.IsReady() && target is Obj_AI_Minion &&
+                    target.IsValidTarget(e.Range) && CommonUtilities.CheckItem())
+                {
+                    CommonUtilities.UseItem();
+                }
+
+            }
+
+
+        }
+
+        private static void OnUpdate(EventArgs args)
+        {
+            if (CommonUtilities.Player.IsDead)
+                return;
+
+            switch (ConfigMenu.orbwalker.ActiveMode)
+            {
+                case Orbwalking.OrbwalkingMode.Combo:
+                    ExecuteCombo();
+                    break;
+
+                case Orbwalking.OrbwalkingMode.Mixed:
+                    LastHit();
+                    ExecuteHarass();
+                    break;
+
+                case Orbwalking.OrbwalkingMode.LastHit:
+                    LastHit();
+                    break;
+
+                case Orbwalking.OrbwalkingMode.LaneClear:
+                    LaneClear();
+                    JungleClear();
+                    break;
+            }
+
+            AutoR();
+            AutoQ();
+            KillSteal();
+        }
+
+        private static void ExecuteCombo()
+        {
+            var target = TargetSelector.GetTarget(q.Range, TargetSelector.DamageType.Magical);
+
+            if (target == null || !target.IsValid)
+                return;
+
+            var enemyCount = Utility.CountEnemiesInRange(q.Range);
+
+            var castQ = ConfigMenu.config.Item("useQ").GetValue<bool>() && q.IsReady();
+            var castW = ConfigMenu.config.Item("useW").GetValue<bool>() && w.IsReady();
+
+            var qHealth = ConfigMenu.config.Item("QHealthCombo").GetValue<Slider>().Value;
+            var wHealth = ConfigMenu.config.Item("WHealthCombo").GetValue<Slider>().Value;
+
+            if (castQ && CommonUtilities.Player.HealthPercent >= qHealth && target.IsValidTarget(q.Range))
+            {
+                q.SPredictionCast(target, HitChance.High);
+            }
+
+            if (castW && CommonUtilities.Player.HealthPercent >= wHealth && !IsBurning() && target.IsValidTarget(w.Range * 2))
+            {
+                w.Cast();
+            }
+            else if (castW && IsBurning() && enemyCount == 0)
+            {
+                w.Cast();
+            }
+        }
+
+        private static void ExecuteHarass()
+        {
+            var target = TargetSelector.GetTarget(q.Range, TargetSelector.DamageType.Magical);
+
+            if (target == null || !target.IsValid)
+                return;
+
+            var castQ = ConfigMenu.config.Item("useQHarass").GetValue<bool>() && q.IsReady();
+
+            var qHealth = ConfigMenu.config.Item("useQHarassHP").GetValue<Slider>().Value;
+
+            if (castQ && CommonUtilities.Player.HealthPercent >= qHealth && target.IsValidTarget(q.Range))
+            {
+                q.SPredictionCast(target, HitChance.High);
+            }
+        }
+
+        private static void LastHit()
+        {
+            var castQ = ConfigMenu.config.Item("useQlh").GetValue<bool>() && q.IsReady();
+
+            var qHealth = ConfigMenu.config.Item("useQlhHP").GetValue<Slider>().Value;
+
+            if (!Orbwalking.CanMove(40))
+                return;
+
+            var minions = MinionManager.GetMinions(CommonUtilities.Player.ServerPosition, q.Range, MinionTypes.All, MinionTeam.NotAlly);
+
+            if (minions.Count > 0 && castQ && CommonUtilities.Player.HealthPercent >= qHealth)
+            {
+                foreach (var minion in minions)
+                {
+                    if (ConfigMenu.config.Item("qRange").GetValue<bool>())
+                    {
+                        if (
+                        HealthPrediction.GetHealthPrediction(
+                            minion, (int)(q.Delay + (minion.Distance(CommonUtilities.Player.Position) / q.Speed))) <
+                        CommonUtilities.Player.GetSpellDamage(minion, SpellSlot.Q) && CommonUtilities.Player.Distance(minion) > CommonUtilities.Player.AttackRange * 2)
+                        {
+                            q.Cast(minion);
+                        }
+                    }
+                    else
+                    {
+                        if (
+                        HealthPrediction.GetHealthPrediction(
+                            minion, (int)(q.Delay + (minion.Distance(CommonUtilities.Player.Position) / q.Speed))) <
+                        CommonUtilities.Player.GetSpellDamage(minion, SpellSlot.Q))
+                        {
+                            q.Cast(minion);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void LaneClear()
+        {
+            var castQ = ConfigMenu.config.Item("useQlc").GetValue<bool>() && q.IsReady();
+            var castW = ConfigMenu.config.Item("useWlc").GetValue<bool>() && w.IsReady();
+
+            var qHealth = ConfigMenu.config.Item("useQlcHP").GetValue<Slider>().Value;
+            var wHealth = ConfigMenu.config.Item("useWlcHP").GetValue<Slider>().Value;
+
+            if (!Orbwalking.CanMove(40))
+                return;
+
+            var minions = MinionManager.GetMinions(CommonUtilities.Player.ServerPosition, q.Range);
+
+            if (minions.Count > 0)
+            {
+                if (castQ && CommonUtilities.Player.HealthPercent >= qHealth)
+                {
+                    foreach (var minion in minions)
+                    {
+                        if (
+                            HealthPrediction.GetHealthPrediction(
+                                minion, (int)(q.Delay + (minion.Distance(CommonUtilities.Player.Position) / q.Speed))) <
+                            CommonUtilities.Player.GetSpellDamage(minion, SpellSlot.Q))
+                        {
+                            q.Cast(minion);
+                        }
+                    }
+                }
+
+                if (castW && CommonUtilities.Player.HealthPercent >= wHealth && !IsBurning())
+                {
+                    w.Cast();
+                }
+                else if (castW && IsBurning() && minions.Count == 0)
+                {
+                    w.Cast();
+                }
+            }
+        }
+
+        private static void JungleClear()
+        {
+            var castQ = ConfigMenu.config.Item("useQj").GetValue<bool>() && q.IsReady();
+            var castW = ConfigMenu.config.Item("useWj").GetValue<bool>() && w.IsReady();
+
+            var qHealth = ConfigMenu.config.Item("useQjHP").GetValue<Slider>().Value;
+            var wHealth = ConfigMenu.config.Item("useWjHP").GetValue<Slider>().Value;
+
+            if (!Orbwalking.CanMove(40))
+                return;
+
+            var minionCount = MinionManager.GetMinions(CommonUtilities.Player.ServerPosition, q.Range, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth);
+
+            if (minionCount.Count > 0)
+            {
+                var minion = minionCount[0];
+
+                if (castQ && CommonUtilities.Player.HealthPercent >= qHealth)
+                {
+                    q.Cast(minion);
+                }
+
+                if (castW && CommonUtilities.Player.HealthPercent >= wHealth && !IsBurning())
+                {
+                    w.Cast();
+                }
+                else if (castW && IsBurning() && minionCount.Count == 0)
+                {
+                    w.Cast();
+                }
+            }
+        }
+
+        private static void KillSteal()
+        {
+            if (!ConfigMenu.config.Item("killsteal").GetValue<bool>())
+                return;
+
+            if (ConfigMenu.config.Item("useQks").GetValue<bool>() && q.IsReady())
+            {
+
+                foreach (var target in HeroManager.Enemies.Where(enemy => enemy.IsValidTarget(q.Range) && !enemy.HasBuffOfType(BuffType.Invulnerability)).Where(target => target.Health < CommonUtilities.Player.GetSpellDamage(target, SpellSlot.Q)))
+                {
+                    q.SPredictionCast(target, HitChance.High);
+                }
+            }
+
+            if (ConfigMenu.config.Item("useIks").GetValue<bool>() && ignite.Slot.IsReady() && ignite != null && ignite.Slot != SpellSlot.Unknown)
+            {
+
+                foreach (var target in HeroManager.Enemies.Where(enemy => enemy.IsValidTarget(ignite.SData.CastRange) && !enemy.HasBuffOfType(BuffType.Invulnerability)).Where(target => target.Health < CommonUtilities.Player.GetSummonerSpellDamage(target, Damage.SummonerSpell.Ignite)))
+                {
+                    CommonUtilities.Player.Spellbook.CastSpell(ignite.Slot, target);
+                }
+            }
+        }
+
+        private static bool IsBurning()
+        {
+            return CommonUtilities.Player.HasBuff("BurningAgony");
+        }
+
+        private static void AutoQ()
+        {
+            if (CommonUtilities.Player.IsDead)
+                return;
+
+            var autoQ = ConfigMenu.config.Item("autoQ").GetValue<KeyBind>().Active && q.IsReady();
+
+            var qHealth = ConfigMenu.config.Item("autoQhp").GetValue<Slider>().Value;
+
+            var target = TargetSelector.GetTarget(q.Range, TargetSelector.DamageType.Magical);
+
+            if (autoQ && CommonUtilities.Player.HealthPercent >= qHealth && target.IsValidTarget(q.Range))
+            {
+                q.SPredictionCast(target, HitChance.High);
+            }
+        }
+
+        private static void AutoR()
+        {
+            if (CommonUtilities.Player.IsDead)
+                return;
+
+            var castR = ConfigMenu.config.Item("useR").GetValue<bool>() && r.IsReady();
+
+            var rHealth = ConfigMenu.config.Item("RHealth").GetValue<Slider>().Value;
+            var rEnemies = ConfigMenu.config.Item("RHealthEnemies").GetValue<bool>();
+
+            var enemyCount = Utility.CountEnemiesInRange(q.Range * 2);
+
+            if (rEnemies && castR && CommonUtilities.Player.HealthPercent <= rHealth && !CommonUtilities.Player.InFountain())
+            {
+                if (enemyCount > 0)
+                {
+                    r.Cast();
+                }
+            }
+            else if (!rEnemies && castR && CommonUtilities.Player.HealthPercent <= rHealth && !CommonUtilities.Player.InFountain())
+            {
+                r.Cast();
+            }
+        }
+    }
+}
---
+++ Mundo/Mundo/Spells.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Mundo
+{
+    internal class Spells
+    {
+        public static Spell q, w, e, r;
+
+        public static SpellDataInst ignite;
+
+        public static void InitializeSpells()
+        {
+            try
+            {
+                q = new Spell(SpellSlot.Q, 1050);
+                w = new Spell(SpellSlot.W, 325);
+                e = new Spell(SpellSlot.E, Orbwalking.GetRealAutoAttackRange(CommonUtilities.Player));
+                r = new Spell(SpellSlot.R);
+
+                q.SetSkillshot(0.25f, 60, 2000, true, SkillshotType.SkillshotLine);
+
+                ignite = CommonUtilities.Player.Spellbook.GetSpell(CommonUtilities.Player.GetSpellSlot("summonerdot"));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Could not load the spells - {0}", exception);
+            }
+        }
+
+    }
+}
