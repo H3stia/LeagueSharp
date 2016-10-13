c1e6bbcf3b5ab05e0b8ac7fec7fd897c64ce6ca0
H3stia <hestia.smod@protonmail.ch>
2016-10-13 19:01:17 +0200

    Kennen Rework

---
+++ Kennen/Kennen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LeagueSharp.Common;
+
+namespace Kennen
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            try
+            {
+                // ReSharper disable once ObjectCreationAsStatement
+                new Champion.Kennen();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Could not load assembly: {0}", exception);
+                throw;
+            }
+        }
+    }
+}
